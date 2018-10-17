0059a490a79c29ac74c2777f4c8ef3b3f4b4d5df
Joe Quattrone <joequattrone5@gmail.com>
2018-10-17 13:58:23 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,14 @@ def prime?(n)
   for i in 2...n do
       if n % i ==  0 
        return false
-  
     end
   end 
+  return true
+  
+  for i in -2...-n do
+    if n % i == 0
+      return false
+    end
+  end
   return true
 end
